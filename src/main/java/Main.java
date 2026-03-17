eae6fa773a74abe2013300adc6fcc82de994063b
Gavin McDaniel <me@gavinmcdaniel.com>
2026-03-17 00:37:30 -0400

    codecrafters submit [skip ci]

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,34 +1,55 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Scanner;
 
 public class Main {
+    static final List<String> validCommands = new ArrayList<>(List.of("exit", "echo", "type"));
+
+    private static void type(String command) {
+        if (validCommands.contains(command)) {
+            System.out.printf("%s is a shell builtin\n", command);
+        } else {
+            System.out.printf("%s: not found\n", command);
+        }
+    }
+
+    private static void echo(String[] arguments) {
+        for (int i = 0; i < arguments.length; i++) {
+            if (i > 0) System.out.print(" ");
+            System.out.print(arguments[i]);
+        }
+        System.out.println();
+    }
+
     public static void main(String[] args) throws Exception {
         Scanner sc = new Scanner(System.in);
 
         while (true) {
             System.out.print("$ ");
-            String userInput = sc.nextLine();
 
-            String[] command = userInput.split(" ");
+            String[] userInput = sc.nextLine().split(" ");
+            String command = userInput[0];
+            String[] arguments = Arrays.copyOfRange(userInput, 1, userInput.length);
 
-            switch (command[0]) {
+            switch (command) {
                 case "exit": {
                     System.exit(0);
                     break;
                 }
 
                 case "echo": {
-                    if (command.length > 1) {
-                        for (int i = 1; i < command.length; i++) {
-                            System.out.print(command[i] + " ");
-                        }
+                    echo(arguments);
+                    break;
+                }
 
-                        System.out.println();
-                    }
+                case "type": {
+                    type(command);
                     break;
                 }
 
                 default: {
-                    System.out.printf("%s: command not found\n", userInput);
+                    System.out.printf("%s: command not found\n", command);
                 }
             }
         }
