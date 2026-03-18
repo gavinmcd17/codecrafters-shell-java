636f3dd477785135a60ae458fdff5c81b5b11599
Gavin McDaniel <me@gavinmcdaniel.com>
2026-03-18 10:21:53 -0400

    codecrafters submit [skip ci]

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -29,15 +29,50 @@ public class Main {
                 }
 
                 default: {
+                    tryRun(command, arguments);
                     System.out.printf("%s: command not found\n", command);
                 }
             }
         }
     }
 
+    /**
+     * Given a program name, find the associated executable file in PATH.
+     *
+     * @param programName name of a program in PATH
+     * @return File object if program exists, otherwise null
+     */
+    private static File findExecutable(String programName) {
+        String[] paths = System.getenv("PATH").split(File.pathSeparator);
+
+        for (String path : paths) {
+            File program = new File(path, programName);
+
+            if (program.exists() && program.canExecute()) {
+                return program;
+            }
+        }
+
+        return null;
+    }
+
+    private static void tryRun(String programName, String[] arguments) throws Exception {
+        File program = findExecutable(programName);
+
+        if (program != null) {
+            List<String> command = new ArrayList<>();
+            command.add(program.getAbsolutePath()); // executable file
+            command.addAll(Arrays.asList(arguments)); // user args
+
+            ProcessBuilder pb = new ProcessBuilder(command);
+            pb.inheritIO();
+            Process process = pb.start();
+            process.waitFor();
+        }
+    }
+
     private static String type(String[] arguments) {
         Set<String> validCommands = Set.of("exit", "echo", "type");
-        String[] paths = System.getenv("PATH").split(File.pathSeparator);
 
         if (arguments.length > 0) {
             String toCheck = arguments[0];
@@ -45,12 +80,10 @@ public class Main {
             if (validCommands.contains(toCheck)) {
                 return String.format("%s is a shell builtin\n", toCheck);
             } else {
-                for (String path : paths) {
-                    File file = new File(path, toCheck);
+                File program = findExecutable(toCheck);
 
-                    if (file.exists() && file.canExecute()) {
-                        return String.format("%s is %s\n", toCheck, file.getAbsolutePath());
-                    }
+                if (program != null) {
+                    return String.format("%s is %s\n", toCheck, program.getAbsolutePath());
                 }
 
                 return String.format("%s: not found\n", toCheck);
@@ -64,10 +97,7 @@ public class Main {
         StringBuilder toEcho = new StringBuilder();
 
         for (int i = 0; i < arguments.length; i++) {
-            if (i > 0) {
-                toEcho.append(' ');
-            }
-
+            if (i > 0) toEcho.append(' ');
             toEcho.append(arguments[i]);
         }
 
